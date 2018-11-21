3800c535c4cec8224cda7894af288f54bfab5dfe
FoxGeoff <fox.geoffrey@gmail.com>
2018-11-20 17:04:45 -0800

    Add WeightEntities Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WeightEntriesService } from './shared/weight-entries.service';
 
 
 @Component({
@@ -9,7 +10,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   
   
-  constructor() {}
+  constructor(private entriesSvc: WeightEntriesService) {}
 
   ngOnInit() {
   }
